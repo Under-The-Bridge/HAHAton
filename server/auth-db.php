de2e1373013bfd4c4173c10b02f2837d196dfbca
Under-The-Bridge <ramazanikbaev6@gmail.com>
2026-04-07 10:20:58 +0500

    result reg and auth

--- server/auth-db.php
+++ server/auth-db.php
@@ -2,10 +2,18 @@
 session_start();
 require "../components/conn.php";
 
-$login = $_POST["email"];
-$password = $_POST["password"];
+$login = mysqli_real_escape_string($conn, $_POST["email"] ?? '');
+$password = $_POST["password"] ?? '';
 
-$sql_check_user = mysqli_query($conn, "SELECT * FROM `users` WHERE `username` = $login");
+if (empty($login) || empty($password)) {
+    echo "<script>
+    alert('Заполните все поля!'); 
+    location.href = '../auth.php';
+    </script>";
+    exit;
+}
+
+$sql_check_user = mysqli_query($conn, "SELECT * FROM `users` WHERE `username` = '$login'");
 
 if (mysqli_num_rows($sql_check_user) > 0) {
     $user = mysqli_fetch_assoc($sql_check_user);
@@ -20,12 +28,14 @@ if (mysqli_num_rows($sql_check_user) > 0) {
     alert('Логин или пароль неверный!');
     location.href = '../auth.php';
     </script>";
+    exit;
     }
 } else {
     echo "<script>
     alert('Пользователь не найден!');
     location.href = '../auth.php';
     </script>";
+    exit;
 }
 
 ?>
--- server/reg-db.php
+++ server/reg-db.php
@@ -1,31 +1,44 @@
 <?php
 require "../components/conn.php";
 
-$login = $_POST["email"] ?? false;
-$fio = $_POST["fio"] ?? false;
-$password = $_POST["password"] ?? false;
+$login = mysqli_real_escape_string($conn, $_POST["email"] ?? '');
+$fio = mysqli_real_escape_string($conn, $_POST["fio"] ?? '');
+$password = $_POST["password"] ?? '';
 $password_user = password_hash($password, PASSWORD_DEFAULT);
 
-$sql_check = mysqli_query($conn, "SELECT * FROM `users` where `username` = $login");
+$sql_check = mysqli_query($conn, "SELECT * FROM `users` where `username` = '$login'");
 if (mysqli_num_rows($sql_check) > 0) {
     echo "<script>
     alert('Пользователь с данной почтой уже существует!');
     location.href = '../reg.php';
     </script>";
-} elseif (trim($login) == "" or trim($password) == "" or trim($fio) == "") {
+    exit;
+} elseif (empty($login) or empty($password) or empty($fio)) {
     echo "<script>
     alert('Не должно быть пустых полей');
     location.href = '../reg.php';
     </script>";
-} elseif (trim($password) < 6) {
+    exit;
+} elseif (strlen(trim($password)) < 6) {
     echo "<script>
     alert('Пароль должен быть более 6 символов!');
     location.href = '../reg.php';
     </script>";
+    exit;
 } else {
-    echo "<script>
+    $sql_add_user = mysqli_query($conn, "INSERT INTO users(`username`, `password`, `fio`) values('$login','$password_user','$fio')");
+    if ($sql_add_user) {
+        echo "<script>
     alert('Учетная запись создана!');
     location.href = '../auth.php';
     </script>";
+    } else {
+        echo "<script>
+    alert('Ошибка при создании учетной записи!');
+    location.href = '../auth.php';
+    </script>";
+        exit;
+    }
+
 }
 ?>
